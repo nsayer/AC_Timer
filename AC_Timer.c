d0220020a073b7e9eb7af30f9dadcc87ff69093a
Nick Sayer <nsayer@kfu.com>
2018-06-05 09:59:01 -0700

    A bunch of updates
    1. Add support for a "warning" LED. If you push the button during warning time, the timer is reset. This allows you to extend the timer without turning the power off (which wasn't possible before).
    2. Don't skip over 0 with the millis timer. Instead, explicitly check for whether the power or warn bits are on to make decisions.

--- AC_Timer.c
+++ AC_Timer.c
@@ -44,17 +44,22 @@
 // Turn on the "warning" light 5 minutes before the end
 #define WARN_TIME (25 * 60 * 1000UL)
 
+// the two output bits
+// POWER is the optoisolator for the AC power
+#define BIT_POWER (_BV(0))
+// WARN is the LED that indicates time is low. Pushing the button once during warning
+// time will just reset the timer without turning off the power.
+#define BIT_WARN (_BV(2))
+
 volatile unsigned long millis_cnt;
 volatile unsigned int cycle_pos;
 
 unsigned long power_on_time;
 unsigned long debounce_start;
+unsigned char in_debounce;
 unsigned char button_state;
 
-// This is a millisecond counter... but 0 is not allowed as a value.
-// This can mean that time measurements might be off by 1 ms under
-// certain circumstances, but that's not significant, and it makes
-// it a lot easier to define 0 as a sort of "NULL" equivalent.
+// This is a millisecond counter.
 static inline unsigned long millis() {
 	unsigned long out;
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
@@ -64,7 +69,7 @@ static inline unsigned long millis() {
 }
 
 ISR(TIM0_COMPA_vect) {
-	if (++millis_cnt == 0) millis_cnt++; // it's not allowed to be zero.
+	millis_cnt++;
 	if (cycle_pos++ >= LONG_CYCLES) {
 		OCR0A = BASE + 1;
 	} else {
@@ -75,15 +80,16 @@ ISR(TIM0_COMPA_vect) {
 
 unsigned char check_button() {
 	unsigned long now = millis();
-	if (debounce_start != 0 && now - debounce_start < DEBOUNCE_TIME) {
+	if (in_debounce && (now - debounce_start) < DEBOUNCE_TIME) {
 		return 0;
 	}
-	debounce_start = 0;
+	in_debounce = 0;
 	unsigned char button = !(PINB & _BV(1)); // 0 means down
 	if (button == button_state) {
 		return 0; // nothing has changed
 	} else {
 		debounce_start = now; // start debouncing
+		in_debounce = 1;
 		button_state = button;
 		return button != 0;
 	}
@@ -110,9 +116,8 @@ void __ATTR_NORETURN__ main(void) {
 	PORTB = _BV(1); // pull-up the button, turn off the outputs
 	DDRB = _BV(0) | _BV(2); // opto and warn LEDs output
 
-	debounce_start = 0;
+	in_debounce = 0;
 	button_state = 0;
-	power_on_time = 0;
 
 	sei(); // turn on interrupts
 
@@ -122,29 +127,32 @@ void __ATTR_NORETURN__ main(void) {
 		unsigned long now = millis();
 
 		if (check_button()) {
-			if (power_on_time) {
+			if (PORTB & BIT_POWER) {
 				// power is on
-				power_on_time = 0;
-				PORTB &= ~(_BV(0) | _BV(2)); // turn it off (and warn too)
+				if (PORTB & BIT_WARN) { // warn is turned on
+					power_on_time = now; // reset the timer only
+					PORTB &= ~BIT_WARN; // turn off warn
+				} else {
+					PORTB &= ~(BIT_POWER | BIT_WARN); // turn it off (and warn too)
+				}
 				continue;
 			} else {
 				// schedule the turn-off
 				power_on_time = now;
-				PORTB |= _BV(0); // turn it on
+				PORTB |= BIT_POWER; // turn it on
 				continue;
 			}
 		}
 
 		// Are we there yet?
-		if (power_on_time != 0 && ((now - power_on_time) > POWER_OFF_TIME)) {
-			power_on_time = 0;
-			PORTB &= ~(_BV(0) | _BV(2)); // turn it off (and warn too)
+		if ((PORTB & BIT_POWER) && ((now - power_on_time) > POWER_OFF_TIME)) {
+			PORTB &= ~(BIT_POWER | BIT_WARN); // turn it off (and warn too)
 			continue;
 		}
 
 		// If the power is on, and it's past the warning time and the WARN light is off...
-		if (power_on_time != 0 && ((now - power_on_time) > WARN_TIME) && (!(PORTB & _BV(2)))) {
-			PORTB |= _BV(2); // turn on the warning light
+		if (((PORTB & (BIT_POWER | BIT_WARN)) == BIT_POWER) && ((now - power_on_time) > WARN_TIME)) {
+			PORTB |= BIT_WARN; // turn on the warning light
 			continue;
 		}
 	}
